e3f6f162115647ba5a80b73429d86afadbc34b7b
unknown <77kangsan60@gmail.com>
2026-04-04 17:06:13 +0900

    CORS ERROR FIXED

--- src/main/java/com/spring_ai_rag/config/WebConfig.java
+++ src/main/java/com/spring_ai_rag/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.spring_ai_rag.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${front.url}")
+    private String frontUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // /api/ 로 시작하는 모든 엔드포인트에 적용
-                .allowedOrigins("http://localhost:3000") // 프론트엔드 주소 허용
+                .allowedOrigins("http://localhost:3000", frontUrl) // 프론트엔드 주소 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS(Preflight) 요청 필수 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키나 인증 정보가 필요할 경우 true
